c92ce33f0a86058d0c5e525dbe39d50131802bc1
Jeff Eng <jeff@jeffeng.me>
2016-08-22 15:58:52 -0700

    created the Capital class object with basic title, info, and coordinate properties, along with basic initializer.

---
+++ Project19/Project19/Capital.swift
@@ -0,0 +1,24 @@
+//
+//  Capital.swift
+//  Project19
+//
+//  Created by Jeffrey Eng on 8/22/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+   
+    var title: String?
+    var info: String
+    var coordinate: CLLocationCoordinate2D
+    
+    //Create basic initializer
+    init(title: String, info: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.info = info
+        self.coordinate = coordinate
+    }
+}
