cdff7014c5cb35e4d175d00b5357a9392b4bd743
Jeff Eng <jeff@jeffeng.me>
2016-08-22 15:42:03 -0700

    initial set up of project. added map view, created outlet, set view controller to be delegate of map view.

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Jeffrey Eng on 8/22/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
