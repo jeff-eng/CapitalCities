26e152660d430f6c9496b537710ecb0f5f362559
Jeff Eng <jeff@jeffeng.me>
2016-08-22 17:37:50 -0700

    map now shows pins of the capital city objects and the city name

--- Project19/Project19/Capital.swift
+++ Project19/Project19/Capital.swift
@@ -11,11 +11,12 @@ import UIKit
 
 class Capital: NSObject, MKAnnotation {
    
+    // Properties
     var title: String?
     var info: String
     var coordinate: CLLocationCoordinate2D
     
-    //Create basic initializer
+    // Create basic initializer
     init(title: String, info: String, coordinate: CLLocationCoordinate2D) {
         self.title = title
         self.info = info
--- Project19/Project19/ViewController.swift
+++ Project19/Project19/ViewController.swift
@@ -15,7 +15,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let london = Capital(title: "London", info: "Home to the 2012 Summer Olympics.", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275))
+        let oslo = Capital(title: "Oslo", info: "Founded over a thousand years ago.", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75))
+        let paris = Capital(title: "Paris", info: "Often called the City of Light.", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508))
+        let rome = Capital(title: "Rome", info: "Has a whole country inside it.", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5))
+        let washington = Capital(title: "Washington, D.C.", info: "Named after George himself.", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667))
+        
+        mapView.addAnnotations([london, oslo, paris, rome, washington])
     }
 
     override func didReceiveMemoryWarning() {
